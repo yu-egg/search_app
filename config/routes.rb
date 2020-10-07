bae110891d457f833af6ff96d7b23ac1537f7f5e
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-07 17:05:00 +0900

    % rails g controller products index searchの実行、今回は2つのアクションしか使わないので、最後に「index」「search」とアクションを指定しています。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'products/index'
+  get 'products/search'
   root 'products#index'
   get 'products/search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
